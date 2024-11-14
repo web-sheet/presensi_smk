34e511fc28bf0ad43999a0a39098f3b39ea7075c
web-sheet <websheetweb@gmail.com>
2024-11-14 09:23:03 +0700

    Update main.js

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ const io = new Server(server, {
     transports: ['websocket', 'polling'] 
 });
 
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 4000; 
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
